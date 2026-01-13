82b9a5cf087210a30d26668a66e889836ebb6563
tochratana <ltomusic69@gmail.com>
2026-01-13 13:49:56 +0700

    add: init

--- vars/sendTelegrameMessage.groovy
+++ vars/sendTelegrameMessage.groovy
@@ -3,6 +3,6 @@ def call(String message, String token, String chatId) {
     curl -s -X POST "https://api.telegram.org/bot${token}/sendMessage" \
     -d chat_id=${chatId} \
     -d text="${message}" \
-    -d parse_mode=MarkdownV2
+    -d parse_mode=HTML
     """
 }
