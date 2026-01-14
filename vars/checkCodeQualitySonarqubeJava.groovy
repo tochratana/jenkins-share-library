d835abc65591fdb453a5b94f4979efe552f7a290
Toch Ratana <159990218+tochratana@users.noreply.github.com>
2026-01-14 08:47:54 +0700

    add sonarqube for java

---
+++ vars/checkCodeQualitySonarqubeJava.groovy
@@ -0,0 +1,18 @@
+def call(String projectName, String projectKey, String projectVersion) {
+    def scannerHome = tool 'sonarqube-scanner'
+
+    withSonarQubeEnv(
+        credentialsId: 'SONARQUBE-TOKEN',
+        installationName: 'sonarqube-scanner'
+    ) {
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=${projectKey} \
+            -Dsonar.projectName="${projectName}" \
+            -Dsonar.projectVersion=${projectVersion} \
+            -Dsonar.sources=src/main/java \
+            -Dsonar.java.binaries=build/classes/java/main \
+            -Dsonar.exclusions=**/test/**,**/build/**
+        """
+    }
+}
